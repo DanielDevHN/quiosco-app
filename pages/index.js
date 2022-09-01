aaa24649734c1e2063e812b4a8052279134cbfbc
DanielDevHN <dangrereyes@gmail.com>
2022-09-01 15:04:00 -0600

    Agregando método para consultar la BD

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({categorias}) {
+  console.log(categorias)
   return (
     <h1>Next.js</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+
+
+  return {
+    props: {
+      categorias,
+    }
+  }
 }
