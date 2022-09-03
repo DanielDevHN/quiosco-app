2f5d543ca456abc50ac20f4805f7012ad0618e51
DanielDevHN <dangrereyes@gmail.com>
2022-09-03 01:09:30 -0600

    Se agregó sidebar para la pantalla principal de la app

---
+++ components/Sidebar.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import useQuiosco from '../hooks/useQuiosco'
+import Categoria from './Categoria'
+
+
+const Sidebar = () => {
+
+    const {categorias} = useQuiosco()
+
+  return (
+    <>
+      <Image width={300} height={100} src="/assets/img/logo.svg"
+        alt='Imagen logotipo' />
+
+        <nav className='mt-10'>
+            {categorias.map(categoria => (
+                <Categoria 
+                    key={categoria.id}
+                    categoria={categoria}
+                />
+            ))}
+        </nav>
+    </>
+  )
+}
+
+export default Sidebar
