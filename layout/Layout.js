a1980acda42b0941871a3908eaf19fc7ea9ae40e
DanielDevHN <dangrereyes@gmail.com>
2022-09-03 01:07:10 -0600

    Se agregó layout principal para l a app

---
+++ layout/Layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+
+export default function Layout({children, pagina }) {
+    return (
+        <>
+            <Head>
+                <title>Café - {pagina}</title>
+                <meta name="description" content="Quiosco Cafeteria"/>
+            </Head>
+
+            <div className="md:flex">
+                <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+                    <Sidebar />
+                </aside>
+
+                <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">{children}</main>
+            </div>
+        </>
+    )
+  }
