dcad89621ee74d916d6403ba01c67e79cf600dbe
DanielDevHN <dangrereyes@gmail.com>
2022-09-24 23:02:28 -0600

    Se agregó titulo y descripción al header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../layout/Layout'
+import useQuiosco from '../hooks/useQuiosco'
 
 export default function Home() {
+
+  const { categoriaActual } = useQuiosco()
+
   return (
-    <Layout>
-      <h1>Inicio</h1>
+    <Layout pagina={`Menu ${categoriaActual?.nombre}`}>
+      <h1 className='text-4xl font-black'>{categoriaActual?.nombre}</h1>
+      <p className='text-2xl my-10'>
+        Elige y personaliza tu pedido a continuación:
+      </p>
     </Layout>
   )
 }
