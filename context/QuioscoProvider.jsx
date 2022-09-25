9b3c649146736973cd4501e1c19fcf672807d794
DanielDevHN <dangrereyes@gmail.com>
2022-09-25 00:54:37 -0600

    Se agregaron métodos para seleccionar productos y mostrar modal

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -8,6 +8,8 @@ const QuioscoProvider = ({children}) => {
 
     const [categorias, setCategorias] = useState([])
     const [categoriaActual, setCategoriaActual] = useState({})
+    const [producto, setProducto] = useState({})
+    const [modal, setModal] = useState(false)
 
     const obtenerCategorias = async () => {
         const { data } = await axios('/api/categorias')
@@ -27,12 +29,24 @@ const QuioscoProvider = ({children}) => {
         setCategoriaActual(categoria[0])
     }
 
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+
+    const handleChangeModal = () => {
+        setModal(!modal)
+    }
+
     return(
         <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria,
+                producto,
+                handleSetProducto,
+                modal,
+                handleChangeModal
             }}
         >
             {children}
