d526b0dc09b0c58a86db49efdc6e7042c839e9b7
DanielDevHN <dangrereyes@gmail.com>
2022-09-25 00:55:18 -0600

    Se agregó modal y estilos para el mismo

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,26 +1,48 @@
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar'
+import Head from "next/head";
+import Modal from "react-modal";
+import Sidebar from "../components/Sidebar";
+import ModalProducto from "../components/ModalProducto";
+import useQuiosco from "../hooks/useQuiosco";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-export default function Layout({children, pagina }) {
-    return (
-        <>
-            <Head>
-                <title>Café - {pagina}</title>
-                <meta name="description" content="Quiosco Cafeteria"/>
-            </Head>
+Modal.setAppElement('#__next');
 
-            <div className="md:flex">
-                <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
-                    <Sidebar />
-                </aside>
+export default function Layout({ children, pagina }) {
+    const { modal } = useQuiosco();
+  return (
+    <>
+      <Head>
+        <title>Café - {pagina}</title>
+        <meta name="description" content="Quiosco Cafeteria" />
+      </Head>
 
-                <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-                    <div className="p-10">
-                        {children}
-                    </div>
-                </main>
-            </div>
-        </>
-    )
-  }
+      <div className="md:flex">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+          <Sidebar />
+        </aside>
+
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          <div className="p-10">{children}</div>
+        </main>
+      </div>
+      {modal && (
+        <Modal
+            isOpen={modal}
+            style={customStyles}
+        >
+            <ModalProducto />
+        </Modal>
+      )}
+    </>
+  );
+}
