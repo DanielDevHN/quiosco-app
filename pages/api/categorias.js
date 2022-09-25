75baea6ff4da37a2414060750a21b0641c8faf34
DanielDevHN <dangrereyes@gmail.com>
2022-09-24 23:44:43 -0600

    Se agregó filtro a endpoint categorías en api

--- pages/api/categorias.js
+++ pages/api/categorias.js
@@ -5,6 +5,10 @@ const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
 
-  const categorias = await prisma.categoria.findMany()
+  const categorias = await prisma.categoria.findMany({
+    include : {
+      productos : true
+    }
+  })
   res.status(200).json(categorias)
 }
