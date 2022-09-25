74bf1eedf43cb362b793b3739a131411bde6c69d
DanielDevHN <dangrereyes@gmail.com>
2022-09-24 23:48:02 -0600

    Se agregó grid par mostrar los productos en el layout

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../layout/Layout'
-import useQuiosco from '../hooks/useQuiosco'
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../layout/Layout";
+import Producto from "../components/Producto";
+import useQuiosco from "../hooks/useQuiosco";
 
 export default function Home() {
-
-  const { categoriaActual } = useQuiosco()
+  const { categoriaActual } = useQuiosco();
 
   return (
     <Layout pagina={`Menu ${categoriaActual?.nombre}`}>
-      <h1 className='text-4xl font-black'>{categoriaActual?.nombre}</h1>
-      <p className='text-2xl my-10'>
+      <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
+      <p className="text-2xl my-10">
         Elige y personaliza tu pedido a continuación:
       </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {categoriaActual?.productos?.map((producto) => (
+          <Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
     </Layout>
-  )
+  );
 }
