12ccf622ace97d252dada8a2714842dd958353fa
DanielDevHN <dangrereyes@gmail.com>
2022-10-04 00:47:49 -0600

    Agregando page para mostrar el resumen del pedido

---
+++ pages/resumen.js
@@ -0,0 +1,13 @@
+import Layout from "../layout/Layout"
+
+
+
+export default function Resumen() {
+
+    return (
+        <Layout pagina="Resumen">
+            <h1 className="text-4xl font-black">Resumen</h1>
+            <p className="text-2xl my-10">Revisa tu Pedido</p>
+        </Layout>
+    )
+}
