8b5e0fe0fdc7b5c5c73944b8aa2849ae1b8b943d
DanielDevHN <dangrereyes@gmail.com>
2022-10-04 00:48:07 -0600

    Agregando page para mostrar el total del pedido

---
+++ pages/total.js
@@ -0,0 +1,11 @@
+import Layout from "../layout/Layout";
+
+
+export default function Total() {
+  return (
+    <Layout pagina="Total y Confirmar Pedido">
+      <h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
+      <p className="text-2xl my-10">Confirma tu Pedido a continuación</p>
+    </Layout>
+  );
+}
