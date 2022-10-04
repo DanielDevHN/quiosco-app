48cbe3e8f76a1709b8474d6bedb8bb6cc2229745
DanielDevHN <dangrereyes@gmail.com>
2022-10-04 00:46:13 -0600

    Agregando barra de progreso del pedido

---
+++ components/NavBar.js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+
+const pasos = [
+  { paso: 1, nombre: "Menú", url: "/" },
+  { paso: 2, nombre: "Resumen", url: "/resumen" },
+  { paso: 3, nombre: "Datos y Total", url: "/total" },
+];
+
+const NavBar = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex justify-between mb-5">
+        {pasos.map((paso) => (
+          <button
+            onClick={() => {
+              router.push(paso.url);
+            }}
+            className="text-2xl font-bold"
+            key={paso.paso}
+          >
+            {paso.nombre}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
